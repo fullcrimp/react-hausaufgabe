dedca27af9d942c2515bb8f4e0e9187617c33dcf
fullcrimp <fullcrimp@protonmail.com>
2018-05-17 21:17:14 +0300

    add more unit tests

--- src/components/Switch/index.test.js
+++ src/components/Switch/index.test.js
@@ -5,13 +5,30 @@ import Switch from './index';
 const text = "testTextString";
 const params = ["date", "rating"];
 
-test('Switch changes the text after click', () => {
-    const wrapper = shallow(<Switch text={ text } params={ params } />);
+describe('Switch renders', () => {
 
-    expect(wrapper.find(".switch__text").text()).toEqual(text);
-    
-    wrapper.find('.switch__param_1').simulate('click');
-    
-    expect(wrapper.find('.switch__param_1').hasClass('switch__param--active')).toBe(true);
+    test('Text in the switch as in passed prop', () => {
+        const wrapper = shallow(<Switch text={ text } params={ params } />);
+
+        expect(wrapper.find(".switch__text").text()).toEqual(text);
+    });
 });
-  
+
+describe('Switch changes', () => {
+
+    test('Switch changes after click #1', () => {
+        const wrapper = shallow(<Switch text={ text } params={ params } />);
+        
+        wrapper.find('.switch__param_1').simulate('click');
+        expect(wrapper.find('.switch__param_1').hasClass('switch__param--active')).toBe(true);
+        expect(wrapper.find('.switch__param_0').hasClass('switch__param--active')).toBe(false);
+    });
+    
+    test('Switch changes after click #0', () => {
+        const wrapper = shallow(<Switch text={ text } params={ params } />);
+
+        wrapper.find('.switch__param_0').simulate('click');
+        expect(wrapper.find('.switch__param_1').hasClass('switch__param--active')).toBe(false);
+        expect(wrapper.find('.switch__param_0').hasClass('switch__param--active')).toBe(true);
+    });
+});
